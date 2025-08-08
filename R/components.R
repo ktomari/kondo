37d26f8603248f619ffcfd28b6cf103ac162d39b
ktomari <ktomari@ucdavis.edu>
2025-08-08 16:21:10 +0900

    added k_dupes which identifies duplicates

--- R/components.R
+++ R/components.R
@@ -14,17 +14,75 @@ k_ls <- function(
   stopifnot(is.character(dir_))
   stopifnot(is.null(pattern_) | is.character(pattern_))
 
-  # obtain list of file in dir, turn into tibble.
-  tb <- tibble::tibble(path = list.files(dir_,
-                                        pattern = pattern_,
-                                        full.names = TRUE),
-                       pattern = pattern_) %>%
-    dplyr::mutate(is_dir = dir.exists(path),
-                  size = ifelse(is_dir,
-                                as.double(NA),
-                                file.size(path)))
+  fl <- list.files(dir_,
+                   pattern = pattern_,
+                   full.names = TRUE,
+                   recursive = TRUE)
 
+  tb <- purrr::map(fl, file.info) %>%
+    dplyr::bind_rows() %>%
+    tibble::rownames_to_column(var = "path") %>%
+    dplyr::mutate(pattern = pattern_) %>%
+    dplyr::select(path, isdir, size, mtime, ctime, atime) %>%
+    dplyr::mutate(size = dplyr::case_when(
+      isdir ~ as.double(NA),
+      .default = size
+    ))
 
   # return
   tb
 }
+
+#' Identify duplicates files in tibble contain columns `path` and `size`.
+#'
+#' @description
+#' If hash is present in the `md5`column, then it is duplicate!
+#'
+#' @param tb
+#'
+#' @returns tibble
+#' @export
+k_dupes <- function(tb){
+  stopifnot(is.data.frame(tb))
+  stopifnot("path" %in% names(tb))
+  stopifnot("size" %in% names(tb))
+
+  # add column that recognizes duplicate filenames
+  tb <- tb %>%
+    dplyr::mutate(filename = stringr::str_extract(path, "[^/]+$")) %>%
+    dplyr::mutate(name_dupe = duplicated(filename) |
+                    duplicated(filename,
+                               fromLast = TRUE))
+
+  # Group by size first
+  potential_dups <- tb %>%
+    dplyr::select(path, size) %>%
+    dplyr::filter(!is.na(size)) %>%
+    dplyr::group_by(size) %>%
+    dplyr::filter(dplyr::n() > 1)
+
+  # Calculate hashes only for potential duplicates
+  potential_dups <- potential_dups %>%
+    dplyr::mutate(
+      md5 = purrr::map_chr(
+      path,
+      ~digest::digest(.x,
+                      algo = "md5",
+                      file = TRUE))) %>%
+    dplyr::group_by(size, md5) %>%
+    dplyr::filter(n() > 1) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(-size)
+
+  # return
+  tb %>%
+    # add dupes b
+    dplyr::left_join(y = potential_dups, by = "path")
+}
+
+# tb <- tibble::tibble(
+#   path = c("~/Downloads/x.csv", "~/Downloads/y.csv"),
+#   size = c(898989, 898989)
+# )
+#
+# k_dupes(tb = tb)
--- R/kondo-package.R
+++ R/kondo-package.R
@@ -2,8 +2,20 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom digest digest
+#' @importFrom dplyr case_when
+#' @importFrom dplyr filter
+#' @importFrom dplyr group_by
+#' @importFrom dplyr left_join
 #' @importFrom dplyr mutate
+#' @importFrom dplyr n
+#' @importFrom dplyr select
+#' @importFrom dplyr ungroup
 #' @importFrom magrittr %>%
+#' @importFrom purrr map_chr
+#' @importFrom stringr str_extract
+#' @importFrom tibble is_tibble
+#' @importFrom tibble rownames_to_column
 #' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
