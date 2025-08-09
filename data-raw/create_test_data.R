a5a858e3e9f22930fb2895ed0008d9f138c7b537
ktomari <ktomari@ucdavis.edu>
2025-08-09 10:27:31 +0900

    added another sample data that is unique.

--- data-raw/create_test_data.R
+++ data-raw/create_test_data.R
@@ -4,6 +4,7 @@ data("iris")
 
 dat1 <- iris[1, 1, drop = FALSE]
 dat2 <- iris[2, 1, drop = FALSE]
+dat3 <- iris[3, 1, drop = FALSE]
 
 if(!dir.exists("inst/extdata/test_iris")){
   dir.create("inst/")
@@ -15,11 +16,20 @@ if(!dir.exists("inst/extdata/test_iris")){
 # create file 1, using dat1
 write.csv(dat1, "inst/extdata/test_iris/iris5.1.csv")
 
+# We pause for period so we can more easily sort files by time created.
+Sys.sleep(1)
+
 # create copy of file 1
 write.csv(dat1, "inst/extdata/test_iris/iris5.1_copy.csv")
+Sys.sleep(1)
 
 # create file 2, using dat2
 write.csv(dat2, "inst/extdata/test_iris/iris4.9.csv")
+Sys.sleep(1)
 
 # create name duplicate of file 2, using dat2
 write.csv(dat2, "inst/extdata/test_iris/dupe/iris4.9.csv")
+Sys.sleep(1)
+
+# now create a unique and single file.
+write.csv(dat3, "inst/extdata/test_iris/iris4.7.csv")
