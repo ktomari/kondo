2075ae480d506e13eecba575162addf4f8ecf805
ktomari <ktomari@ucdavis.edu>
2025-08-09 13:03:30 +0900

    added functionality to dupe detection: group IDs added, write to Downloads added.

--- R/components.R
+++ R/components.R
@@ -33,18 +33,25 @@ k_ls <- function(
   tb
 }
 
-#' Identify duplicates files in tibble contain columns `path` and `size`.
+#' Identify duplicates files in tibble contain columns path and size
 #'
 #' @description
 #' If hash is present in the `md5`column, then it is duplicate!
 #'
 #' @param tb data.frame/tibble. The data.frame returned by `k_ls`.
 #' @param rm.unique logical. Whether or not to remove unique values. By default, it is `TRUE`.
+#' @param msg_rate numeric. How often do you want a system message to appear as you run the function? By default, on every 50th file, a message will appear.
+#' @param write_ logical. Write csv output to disk. By default it downloads to ~/Downloads.
 #'
 #' @returns tibble
 #' @export
-k_dupes <- function(tb, rm.unique = TRUE){
+k_dupes <- function(tb,
+                    rm.unique = TRUE,
+                    msg_rate = 50,
+                    write_ = FALSE){
   stopifnot(inherits(tb, 'data.frame'))
+  stopifnot(inherits(msg_rate, 'numeric'))
+  stopifnot(inherits(write_, "logical"))
   stopifnot("path" %in% names(tb))
   stopifnot("size" %in% names(tb))
 
@@ -56,37 +63,69 @@ k_dupes <- function(tb, rm.unique = TRUE){
                                fromLast = TRUE))
 
   # Group by size first
-  potential_dups <- tb %>%
+  dups <- tb %>%
     dplyr::select(path, size) %>%
     dplyr::filter(!is.na(size)) %>%
     dplyr::group_by(size) %>%
-    dplyr::filter(dplyr::n() > 1)
+    dplyr::filter(dplyr::n() > 1) %>%
+    dplyr::ungroup()
 
   # Calculate hashes only for potential duplicates
-  potential_dups <- potential_dups %>%
+  dups <- dups %>%
     dplyr::mutate(
-      md5 = purrr::map_chr(
+      md5 = purrr::imap_chr(
       path,
-      ~digest::digest(.x,
-                      algo = "md5",
-                      file = TRUE))) %>%
-    dplyr::group_by(size, md5) %>%
+      function(x, idx){
+        # check for messages.
+        if(!(msg_rate == 0 | is.na(msg_rate))){
+          if(idx %% msg_rate == 0){
+            message(paste0(
+              "k_dupes progress: ",
+              idx,
+              " of ",
+              nrow(dups),
+              "."
+            ))
+
+          }
+        }
+
+        # return
+        digest::digest(x,
+                       algo = "md5",
+                       file = TRUE)
+      })) %>%
+    dplyr::group_by(md5) %>%
     dplyr::filter(n() > 1) %>%
+    mutate(group_id = dplyr::cur_group_id()) %>%
     dplyr::ungroup() %>%
     dplyr::select(-size)
 
   # add md5 column in to original dataset.
   tb <- tb %>%
     # add dupes b
-    dplyr::left_join(y = potential_dups, by = "path")
+    dplyr::left_join(y = dups, by = "path")
 
   if(rm.unique){
     # create filter criteria of duplicated values.
     filtr_ <- tb$md5[duplicated(tb$md5)] |>
       unique()
 
+    # remove NA
+    filtr_ <- filtr_[!is.na(filtr_)]
+
     # execute filter
     tb <- tb[tb$md5 %in% filtr_,]
+  }
+
+  if(write_){
+    nm <- paste0("~/Downloads/kondo_kdupes_",
+                 format(Sys.time(), "%Y_%m_%d_%H:%M"),
+                 ".csv")
+    write.csv(
+      x = tb,
+      file = nm
+    )
   }
 
   # return
